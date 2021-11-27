584251215e36c2a23fb4917b2243664bbdc6fba8
Ram11Coder <rambeliver11@gmail.com>
2021-11-27 22:24:32 +0530

    Completed #24 #26

--- Problems/Problem24.java
+++ Problems/Problem24.java
@@ -1,5 +1,12 @@
 package com.euler;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * A permutation is an ordered arrangement of objects. For example, 3124 is one
  * possible permutation of the digits 1, 2, 3 and 4. If all of the permutations
@@ -10,14 +17,50 @@ package com.euler;
  * 
  * What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4,
  * 5, 6, 7, 8 and 9?
-
  *
+ * 
  */
 public class Problem24 {
+	/**
+	 * 1.Permutation logic 2.sort the array 3.Find out millionth position
+	 * 
+	 * @throws IOException
+	 * 
+	 */
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new FileWriter("D:\\daily coding track\\permutation.txt"));
+		long past = System.currentTimeMillis();
+		String s = "0123456789";
+		List<String> list = new ArrayList<String>();
+		Collections.sort(permutate(s, 0, s.length(), list));
+		System.out.println((System.currentTimeMillis() - past) / 1000.0 + "Sec");
+		bw.write(list + "");
+		System.out.println(list.get(1_000_000 - 1));
+		bw.close();
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static List<String> permutate(String str, int l, int r, List<String> list) {
+		if (l == r) {
+			list.add(str);
+			// System.out.println(str);
+		} else {
+			for (int i = l; i < r; i++) {
+				str = swap(str, l, i);
+				list = permutate(str, l + 1, r, list);
+				str = swap(str, l, i);
 
+			}
+		}
+
+		return list;
+	}
+
+	private static String swap(String str, int l, int i) {
+		char c[] = str.toCharArray();
+		char temp = c[l];
+		c[l] = c[i];
+		c[i] = temp;
+		return String.valueOf(c);
 	}
 
 }
--- Problems/Problem26.java
+++ Problems/Problem26.java
@@ -1,34 +1,64 @@
 package com.euler;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Reciprocal cycles
- *  
-Problem 26
-
-A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
-
-1/2	= 	0.5
-1/3	= 	0.(3)
-1/4	= 	0.25
-1/5	= 	0.2
-1/6	= 	0.1(6)
-1/7	= 	0.(142857)
-1/8	= 	0.125
-1/9	= 	0.(1)
-1/10	= 	0.1
-Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
-
-Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+ * 
+ * Problem 26
+ * 
+ * A unit fraction contains 1 in the numerator. The decimal representation of
+ * the unit fractions with denominators 2 to 10 are given:
+ * 
+ * 1/2 = 0.5 1/3 = 0.(3) 1/4 = 0.25 1/5 = 0.2 1/6 = 0.1(6) 1/7 = 0.(142857) 1/8
+ * = 0.125 1/9 = 0.(1) 1/10 = 0.1 Where 0.1(6) means 0.166666..., and has a
+ * 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring
+ * cycle.
+ * 
+ * Find the value of d < 1000 for which 1/d contains the longest recurring cycle
+ * in its decimal fraction part.
  *
  */
 public class Problem26 {
-
+//reference : 
 	public static void main(String[] args) {
-		for (double i = 2; i < 1000; i++) {
-			System.out.println(BigDecimal.valueOf(1/i));
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 7; i < 1000; i += 2) {
+			if (isprime(i))
+				list.add(i);
 		}
+
+		int max = 0, index = 0;
+		for (int i = 0; i < list.size(); i++) {
+			int count = repeatableCount(list.get(i));
+			if (max < count) {
+				index = list.get(i);
+				max = count;
+			}
+		}
+
+		System.out.println(index + " : " + max);
+
+	}
+
+	private static int repeatableCount(int b) {
+		int a = 1, c = 0;
+		do {
+			a = a * 10 % b;
+			c++;
+		} while (a != 1);
+		return c;
+	}
+
+	private static boolean isprime(int num) {
+		if (num % 2 == 0 || num % 3 == 0)
+			return false;
+
+		for (int i = 5; i * i <= num; i += 6)
+			if (num % i == 0 || num % (i + 2) == 0)
+				return false;
+		return true;
 	}
 
 }
