4564232224f212994530711022fb84f7595c3e1f
Ram11Coder <rambeliver11@gmail.com>
2021-12-31 00:26:53 +0530

    Added problem #76

--- Problems/Problem76.java
+++ Problems/Problem76.java
@@ -14,5 +14,27 @@ How many different ways can one hundred be written as a sum of at least two posi
  *
  */
 public class Problem76 {
+	static int c;
 
+	public static void main(String[] args) {
+		long past = System.currentTimeMillis();
+		int n = 100;
+		int arr[] = new int[n];
+		summationCount(n, arr, 0, 0);
+		System.out.println(c-1);//Exculde 100 as a count
+		System.out.println((System.currentTimeMillis() - past) / 1000.0 + "sec");
+	}
+
+	private static void summationCount(int n, int[] arr, int curr_sum, int curr_index) {
+		if (n == curr_sum) {
+			c++;
+		} else {
+			int num = 1;
+			while (num <= n - curr_sum && (curr_index == 0 || num <= arr[curr_index - 1])) {
+				arr[curr_index] = num;
+				summationCount(n, arr, curr_sum + num, curr_index + 1);
+				num++;
+			}
+		}
+	}
 }
