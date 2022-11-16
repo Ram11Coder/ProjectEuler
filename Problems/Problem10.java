c0ba01f6154ae856277cb82b3a53870402d8e5a8
Ram11Coder <rambeliver11@gmail.com>
2022-11-16 08:44:07 +0530

    Added Java 8 Solution

--- Problems/Problem10.java
+++ Problems/Problem10.java
@@ -1,5 +1,7 @@
 package com.euler;
 
+import java.util.stream.LongStream;
+
 /**
  * Summation of primes Problem 10
  * 
@@ -10,35 +12,43 @@ package com.euler;
  */
 public class Problem10 {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
+    System.out.println("Traditional Solution :");
+    System.out.println(findSumOfPrimes(2_000_000));
+    System.out.println("Java8 Solution :");
+    System.out.println(findSumOfPrimes_java8(2_000_000));
 
-		System.out.println(findSumOfPrimes(2_000_000));
+  }
 
-	}
+  private static long findSumOfPrimes(int limit) {
+    long sum = 0;
+    for (int i = 2; i < limit; i++)
+      if (isPrime(i))
+        sum += i;
+    return sum;
+  }
 
-	private static long findSumOfPrimes(int limit) {
-		long sum = 0;
-		for (int i = 2; i < limit; i++)
-			if (isPrime(i))
-				sum += i;
-		return sum;
-	}
 
-	private static boolean isPrime(int n) {
-		if (n <= 1)
-			return false;
+  // Java 8
+  private static long findSumOfPrimes_java8(int limit) {
+    return LongStream.range(2, limit).filter(Problem10::isPrime).sum();
+  }
 
-		if (n <= 3)
-			return true;
+  private static boolean isPrime(long n) {
+    if (n <= 1)
+      return false;
 
-		if (n % 2 == 0 || n % 3 == 0)
-			return false;
+    if (n <= 3)
+      return true;
 
-		for (int i = 5; i * i <= n; i += 6)
-			if (n % i == 0 || n % (i + 2) == 0)
-				return false;
+    if (n % 2 == 0 || n % 3 == 0)
+      return false;
 
-		return true;
-	}
+    for (int i = 5; i * i <= n; i += 6)
+      if (n % i == 0 || n % (i + 2) == 0)
+        return false;
+
+    return true;
+  }
 
 }
